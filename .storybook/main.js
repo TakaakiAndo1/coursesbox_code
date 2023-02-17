121776fd3090fab2170a7785abad9575d1f02a5a
Takaaki Ando <z.takaaki.ando@gree.net>
2023-02-17 16:42:55 +0900

    add Button component

--- .storybook/main.js
+++ .storybook/main.js
@@ -3,8 +3,8 @@ module.exports = {
   "stories": [
     '../pages/**/*.stories.mdx',
     '../pages/**/*.stories.@(js|jsx|ts|tsx)',
-    // '../components/**/*.stories.mdx',
-    // '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    '../components/**/*.stories.mdx',
+    '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   "addons": [
     // 'storybook-addon-next',
