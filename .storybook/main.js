bacb8712ad983eaf70b48af4e4ffe86740da9ee9
Takaaki Ando <z.takaaki.ando@gree.net>
2023-02-17 15:05:04 +0900

    2023/02/17時点で動く状態に修正

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,11 +1,13 @@
 module.exports = {
+  staticDirs: ['../public'],
   "stories": [
     '../pages/**/*.stories.mdx',
     '../pages/**/*.stories.@(js|jsx|ts|tsx)',
-    '../components/**/*.stories.mdx',
-    '../components/**/*.stories.@(js|jsx|ts|tsx)',
+    // '../components/**/*.stories.mdx',
+    // '../components/**/*.stories.@(js|jsx|ts|tsx)',
   ],
   "addons": [
+    // 'storybook-addon-next',
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions"
